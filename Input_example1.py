c041bdfc1330ca03799de7a3e80636ced825ea90
Tamer Ozbay <tozbay@biltam.com.tr>
2019-07-23 17:18:03 +0300

    Create Input_example1.py

---
+++ Input_example1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+ -*- coding: utf-8 -*-
+ 
+print ("İki Sayıyı Toplayan Program")
+sayi1 = raw_input("Birinci Sayıyı Giriniz:")
+sayi2 = raw_input("İkinci Sayıyı Giriniz:");
+ 
+toplam = sayi1+sayi2
+print (toplam)
